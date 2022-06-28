2b5d67cf9a2fb1d1928d0dd8f96928fb3799c992
isuel <isuel2@yandex.ru>
2022-06-28 18:58:34 +0600

    added base_page.py, conftest.py, requirements.txt

---
+++ conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture
+def get_chrome_options():
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    options.add_argument('chrome')  # Use headless if you do not need a browser UI
+    # options.add_argument('--start-maximized')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--auto-open-devtools-for-tabs')
+    return options
+
+
+@pytest.fixture
+def get_webdriver(get_chrome_options):
+    options = get_chrome_options
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+
+@pytest.fixture(scope='function')
+def driver(get_webdriver):
+    driver = get_webdriver
+    driver.set_window_size(1920, 1080)
+    yield driver
+    driver.quit()
